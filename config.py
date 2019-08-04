2b93de480b1e28d70d66a490f58f9063ccbff66f
Python-Development <legromant@gmail.com>
2019-08-04 13:42:33 +0300

    test17

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-# class Configuration(object):
-#     DEBUG = True
-#
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp\\finance.db'
+    SECRET_KEY = 'some_secret'
